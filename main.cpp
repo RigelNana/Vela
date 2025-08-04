6b3fe24053ebab62788e8bb85da05c9ab02b445b
RigelNana <809239189@qq.com>
2025-08-04 11:06:18 +0800

    test

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@
 import units;
 import time_sys;
 import coord;
+import transform;
 using namespace vela::units::literals;
 
 int main() {
-    auto sidereal = vela::time::sidereal_time(vela::time::now_jd());
+    auto jd = vela::time::JulianDate::from_calendar(1987, 4, 10, 19, 21, 0.0);
+    auto sidereal = vela::time::sidereal_time(jd);
+    auto geo = vela::coord::geographical(vela::units::make_degrees(-77, 3, 56),
+                                         vela::units::make_degrees(38, 55, 17.0));
+    auto venus = vela::coord::equatorial(vela::units::make_hours(23, 9, 16.641), vela::units::make_degrees(-6, 43, 11.61));
+    auto horizontal = vela::coord::trans::to_horizontal(venus, geo, jd);
+    std::println("Horizontal Coordinates: Azimuth = {}, Altitude = {}",
+                 horizontal.azimuth().value(), horizontal.altitude().value());
     std::println("Sidereal Time: {}", sidereal.value());
-
-
+    auto hour_angle = vela::coord::hour_angle(venus, geo, jd);
+    std::println("Hour Angle: {}", hour_angle.to_degrees().value());
+    auto eq = vela::coord::trans::to_equatorial(horizontal, geo, jd);
+    std::println("Equatorial Coordinates: RA = {}, Dec = {}",
+                 eq.right_ascension().value(), eq.declination().value());
 }
